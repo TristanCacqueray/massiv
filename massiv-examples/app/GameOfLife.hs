2b8b96b48ce66b2c8634669e16fee63021f66b68
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-04 20:31:08 +0000

    Change GameOfLife to a sandpile simulator

--- massiv-examples/app/GameOfLife.hs
+++ massiv-examples/app/GameOfLife.hs
@@ -3,58 +3,47 @@
 {-# LANGUAGE TypeFamilies #-}
 module Main where
 
+import Control.Applicative
 import Control.Monad
 import Data.Massiv.Array as A hiding (windowSize)
 import Data.Massiv.Array.Unsafe as A
+
 import Data.Word
 import Graphics.UI.GLUT as G
 import System.Exit (ExitCode(..), exitSuccess, exitWith)
 import Text.Read (readMaybe)
 
-lifeRules :: Word8 -> Word8 -> Word8
-lifeRules 0 3 = 1
-lifeRules 1 2 = 1
-lifeRules 1 3 = 1
-lifeRules _ _ = 0
 
-lifeStencil :: Stencil Ix2 Word8 Word8
-lifeStencil = makeStencil (Sz (3 :. 3)) (1 :. 1) $ \ f ->
-  lifeRules <$> f (0 :. 0) <*>
-  (f (-1 :. -1) + f (-1 :. 0) + f (-1 :. 1) +
-   f ( 0 :. -1) +               f ( 0 :. 1) +
-   f ( 1 :. -1) + f ( 1 :. 0) + f ( 1 :. 1))
+sandUpdate :: (Ord a, Num a) => (Ix2 -> a) -> a
+sandUpdate f
+  | here > 10    = here
+  | here > 4     = here - 3
+  | otherwise    = here + fromIntegral (length $ filter (>4) [u, d, l, r])
+  where
+    here = f (0 :. 0)
+    u = f ( 0 :.  1)
+    d = f ( 0 :. -1)
+    l = f (-1 :.  0)
+    r = f ( 1 :.  0)
 
-life :: Array S Ix2 Word8 -> Array S Ix2 Word8
-life = compute . A.mapStencil Wrap lifeStencil
+sandStencil :: (Ord a, Num a, Default a) => Stencil Ix2 a a
+sandStencil = makeStencil (Sz (3 :. 3)) (1 :. 1) sandUpdate
 
-initLife :: Sz2 -> Array S Ix2 Word8 -> Array S Ix2 Word8
-initLife sz arr =
+nextSand :: (Ord a, Num a, Default a, Storable a) => Array S Ix2 a -> Array S Ix2 a
+nextSand = compute . A.mapStencil Wrap sandStencil
+
+initSand :: Sz2 -> Array S Ix2 Word16 -> Array S Ix2 Word16
+initSand sz arr =
   compute $
-  insertWindow (A.replicate Par sz 0) (Window ix0 (size arr) (index' arr . subtract ix0) Nothing)
+  insertWindow
+    (makeArrayR D Par sz (const 0))
+    (Window ix0 (size arr) (index' arr . subtract ix0) Nothing)
   where
     ix0 = liftIndex (`div` 2) (unSz (sz - size arr))
 
 
-blinker :: Array S Ix2 Word8
-blinker = [ [0, 1, 0]
-          , [0, 1, 0]
-          , [0, 1, 0] ]
-
-
-glider :: Array S Ix2 Word8
-glider = [ [0, 1, 0]
-         , [0, 0, 1]
-         , [1, 1, 1] ]
-
-inf2 :: Array S Ix2 Word8
-inf2 = [ [1, 1, 1, 0, 1]
-       , [1, 0, 0, 0, 0]
-       , [0, 0, 0, 1, 1]
-       , [0, 1, 1, 0, 1]
-       , [1, 0, 1, 0, 1] ]
-
 -- | Scale the array, negate values and create an image with a grid.
-pixelGrid :: Int -> Array S Ix2 Word8 -> Array D Ix2 Word8
+pixelGrid :: Int -> Array S Ix2 Word16 -> Array D Ix2 Word16
 pixelGrid k8 arr = A.makeArray (getComp arr) sz' getNewElt
   where
     k = succ k8
@@ -62,11 +51,19 @@ pixelGrid k8 arr = A.makeArray (getComp arr) sz' getNewElt
     sz' = Sz (1 + m * k :. 1 + n * k)
     getNewElt (j :. i) =
       if i `mod` k == 0 || j `mod` k == 0
-        then 128
-        else (1 - A.unsafeIndex arr ((i - 1) `div` k :. (j - 1) `div` k)) * 255
+        then 32768
+        else A.unsafeIndex arr ((i - 1) `div` k :. (j - 1) `div` k)
 
 sizeFromSz2 :: Sz2 -> G.Size
 sizeFromSz2 (Sz2 m n) = Size (fromIntegral n) (fromIntegral m)
+
+inf2 :: Array S Ix2 Word16
+inf2 = [ [0, 0, 0, 0, 0]
+       , [0, 0, 0, 0, 0]
+       , [0, 0, 20000, 0, 0]
+       , [0, 0, 0, 0, 0]
+       , [0, 0, 0, 0, 0] ]
+
 
 main :: IO ()
 main = do
@@ -96,7 +93,7 @@ main = do
 startGameOfLife :: Sz2 -> Int -> IO ()
 startGameOfLife sz s = do
   rowAlignment Unpack $= 1
-  let iLife = initLife sz inf2
+  let iLife = initSand sz inf2
       wSz = size (pixelGrid s iLife)
   G.windowSize $= sizeFromSz2 wSz
   mArr <- new wSz
@@ -105,31 +102,32 @@ startGameOfLife sz s = do
   runGameOfLife s mArr iLife
 
 
-drawLife :: Int -> MArray RealWorld S Ix2 Word8 -> Array S Ix2 Word8 -> IO ()
+drawLife :: Int -> MArray RealWorld S Ix2 Word16 -> Array S Ix2 Word16 -> IO ()
 drawLife s mArr arr = do
   computeInto mArr $ pixelGrid s arr
   A.withPtr mArr $ \ptr ->
-    drawPixels (sizeFromSz2 (msize mArr)) (PixelData Luminance UnsignedByte ptr)
+    drawPixels (sizeFromSz2 (msize mArr)) (PixelData Green UnsignedShort ptr)
 
 
-drawLifeStep :: Int -> MArray RealWorld S Ix2 Word8 -> Array D Ix2 (Word8, Word8) -> IO ()
+drawLifeStep :: Int -> MArray RealWorld S Ix2 Word16 -> Array D Ix2 (Word16, Word16) -> IO ()
 drawLifeStep s mArr arr = do
   imapM_ updateCellLife arr
   A.withPtr mArr $ \ptr ->
-    drawPixels (sizeFromSz2 (msize mArr)) (PixelData Luminance UnsignedByte ptr)
+    drawPixels (sizeFromSz2 (msize mArr)) (PixelData Green UnsignedShort ptr)
   where
     k = s + 1
     updateCellLife (i :. j) (prev, next) =
       when (prev /= next) $ do
         let ixArr = makeArrayR D Seq (Sz2 s s) $ \(jc :. ic) -> (1 + jc + j * k) :. (1 + ic + i * k)
-            nVal = (1 - next) * 255
+            nVal = next * (65535 `div` 4)
         A.forM_ ixArr $ \ix -> write mArr ix nVal
 
-runGameOfLife :: Int -> MArray RealWorld S Ix2 Word8 -> Array S Ix2 Word8 -> IO ()
+runGameOfLife :: Int -> MArray RealWorld S Ix2 Word16 -> Array S Ix2 Word16 -> IO ()
 runGameOfLife s mArr = go
   where
     go arr = do
-      let nextLife = life arr
+      let nextLife = nextSand arr
       drawLifeStep s mArr $ A.zip arr nextLife
       flush
-      addTimerCallback 10 $ go nextLife
+      addTimerCallback 50 $
+        go nextLife
